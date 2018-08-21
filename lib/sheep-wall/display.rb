700805b5bb161879aca56cabb8cfcdfba5b85cf2
Nightfeather <ss900328@gmail.com>
2018-08-21 23:41:22 +0800

    Avoid duplicate entry

--- lib/sheep-wall/display.rb
+++ lib/sheep-wall/display.rb
@@ -1,9 +1,19 @@
 module SheepWall
   class Display
+
     def initialize
+      @history = []
+      @timestamps = []
     end
 
     def show entity
+      return if @history.include? entity
+      if ENV["DEBUG"]
+        print "[debug] Accepted: "
+        p entity
+      end
+      @history << entity
+      @timestamps << [ entity, Time.now ]
       case entity
       when Hash
         puts "%s %s %s %s" % [ entity[:type], entity[:client], entity[:host], entity[:cred] ]
@@ -14,3 +24,4 @@ module SheepWall
 
   end
 end
+
